436ade2f5e31fdf8f8541a148ee2390900b4587d
nguyen <nguyen@digital-launch.com>
2023-01-08 15:13:15 +0700

    EA, add plugin

---
+++ app/code/Nguyen/Plugin/ProductRepositoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+namespace Nguyen\ExtensionAttribute\Plugin;
+use Magento\Catalog\Api\Data\ProductRepositoryInterface as MagentoRepository;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+
+class ProductRepositoryInterface
+{
+    protected $_collectionFactory;
+
+    public function __construct(
+        CollectionFactory $collectionFactory
+    ){
+        $this->$_collectionFactory = $collectionFactory;
+    }
+
+    public function afterGet(MagentoRepository $subject, ProductInterface $product){
+
+        if ($product->getExtensionAttributes() && $product->getExtensionAttributes()->getIsHandMade()){
+            return $product;
+        }
+        $isHandMade = $this->getIsHandMade($product->getId());
+    }
+
+    public function getIsHandMade($productId){
+        return $this->collectionFactory->create()
+            ->addFieldToFilter('entity_id', ['eq' => $productId])
+            ->getFirstItem()->getData('is_handmade');
+    }
+}
+
