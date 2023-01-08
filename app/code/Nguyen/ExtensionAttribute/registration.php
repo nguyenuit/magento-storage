349547fb1bc9be3ede4404f14f044c072462cf7a
nguyen <nguyen@digital-launch.com>
2023-01-08 14:47:05 +0700

    extension attribute

---
+++ app/code/Nguyen/ExtensionAttribute/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Nguyen ExtensionAttribute All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Nguyen_ExtensionAttribute', __DIR__);
+
